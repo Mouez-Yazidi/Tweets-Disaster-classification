8dbb8a11f82ff13f51696a43caf1a52c385d0072
Mouez Yazidi <56160970+Mouez2019@users.noreply.github.com>
2022-02-22 03:03:33 +0100

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -17,12 +17,13 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords,wordnet
 import re
 plt.style.use('seaborn')
+"""
 def remove_Stopwords(text):
     stopW=stopwords.words('english') #get the english stopwords
     return " ".join([i for i in text.split() if i not in stopW])
 
 def get_wordnet_pos(word):
-    """Map POS tag to first character lemmatize() accepts"""
+   # """Map POS tag to first character lemmatize() accepts"""
     tag = nltk.pos_tag([word])[0][1][0].upper()
     tag_dict = {"J": wordnet.ADJ,
                 "N": wordnet.NOUN,
@@ -35,6 +36,7 @@ def lemmatize(text):
     lemmatizer = WordNetLemmatizer()
     # 2. Lemmatize text with the appropriate POS tag
     return " ".join([lemmatizer.lemmatize(i, get_wordnet_pos(i)) for i in text.split()])
+"""
 def clean_text(text):
     # convert catacter to lowercase
     text=text.lower()
@@ -51,9 +53,9 @@ def clean_text(text):
     #remove single caracter
     text = re.sub(r'\s+[a-zA-Z]\s+'," ",text)
     #remove stop Words
-    text = remove_Stopwords(text)
+    #text = remove_Stopwords(text)
     # text normalization
-    text = lemmatize(text)
+    #text = lemmatize(text)
     return list(text)
 def text_to_seq(text):
     text = clean_text(text)
